7139ad61d8d10511b133405fe61e38746fb7a000
EMON <mamudmdemon@gmail.com>
2024-05-31 23:10:20 +0600

    navbar design complete

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -19,32 +19,23 @@ const NavBar = () => {
   }, []);
 
   const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="mt-2 mb-4 flex flex-col gap-2 text-black lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
+        className="p-1 font-semibold"
       >
         <a href="#" className="flex items-center">
           Home
         </a>
       </Typography>
       <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
+        className="p-1 font-semibold"
       >
         <a href="#" className="flex items-center">
           Dashboard
         </a>
       </Typography>
       <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
+        className="p-1 font-semibold"
       >
         <a href="#" className="flex items-center">
           Notification icon
@@ -56,9 +47,12 @@ const NavBar = () => {
     <div className=" max-h-[768px] w-full ">
       <Navbar className="sticky  top-0 z-10 h-max max-w-full shadow-none rounded-none px-4 py-2 lg:px-16 lg:py-4">
         <div className="flex items-center  justify-between text-blue-gray-900">
-          <Typography className="mr-4  flex items-center gap-2 font-cinzel text-xl cursor-pointer py-1.5 font-medium">
+          <Typography className="mr-4  flex items-center gap-2 font-cinzel text-3xl text-[#0B2D42] cursor-pointer py-1.5 font-bold">
             <img src={logo} alt="" className="w-16" />
-            SwiftParcel
+            <div className="flex items-center">
+            <p className="text-7xl">S</p>
+            <sub>wift</sub>
+            </div>
           </Typography>
           <div className="mr-4 hidden lg:block">{navList}</div>
           <div className="flex items-center gap-4">
